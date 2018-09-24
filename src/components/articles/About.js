a0a020b7dcb015e46fd5bf735fad1062f8643eb6
Labais <reinitto@gmail.com>
2018-09-24 05:39:45 +0200

    Changed file name

--- src/components/articles/About.js
+++ src/components/articles/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../News.css";
-import normunds from "../../img/nr_3.jpg";
+import normunds from "../../img/nr_31.jpg";
 import ArticleLayout from "./ArticleLayout";
 
 const boldFont = {
