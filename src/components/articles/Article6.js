b46fc5af6a89e02566f45c32c3e411aeb8646442
Labais <reinitto@gmail.com>
2018-09-24 05:37:25 +0200

    Changed file name

--- src/components/articles/Article6.js
+++ src/components/articles/Article6.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Article6.css";
 import img13 from "../../img/13.jpg";
-import graph1 from "../../img/graph1.jpg";
-import graph2 from "../../img/graph2.jpg";
-import graph3 from "../../img/graph3.jpg";
+import graph1 from "../../img/graph11.jpg";
+import graph2 from "../../img/graph21.jpg";
+import graph3 from "../../img/graph31.jpg";
 
 import ArticleLayout from "./ArticleLayout";
 
