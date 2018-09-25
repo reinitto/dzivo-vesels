c689f9247f861d19d1d1979ddd6fe2cfcd06bb39
Labais <reinitto@gmail.com>
2018-09-25 15:06:23 +0200

    Edited navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,73 +13,88 @@ export default () => {
       className="container-fluid border no-wrap"
       style={{ display: "flex", justifyContent: "center" }}
     >
+      <Link to="/">
+        <h4
+          style={{
+            fontWeight: 500,
+            textDecoration: "underline",
+
+            padding: "10px 15px"
+          }}
+        >
+          Sporto Vesels <strong> - </strong>{" "}
+          <b style={{ color: "#026f89" }}>Dzīvo Labāk</b>
+        </h4>
+      </Link>
       <img
         src={nr_31}
         className="img-responsive"
         alt=""
         style={{
-          maxHeight: "100px",
+          maxHeight: "130px",
           display: "inline-block"
         }}
       />
-      <Navbar collapseOnSelect className="white">
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">
-              <h4
-                style={{
-                  fontWeight: 500,
-                  textDecoration: "underline"
-                }}
+      <div className="container-fluid">
+        <Navbar collapseOnSelect className="white">
+          <Navbar.Header>
+            <Navbar.Brand />
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight className=" navbar-right">
+              <NavItem eventKey={1} href="/Trenini">
+                <h4>Individuālie treniņi</h4>
+              </NavItem>
+              <NavItem eventKey={3} href="/Konsultacijas">
+                <h4>Konsultācijas</h4>
+              </NavItem>
+              <NavItem eventKey={3} href="/Seminari">
+                <h4>Semināri</h4>
+              </NavItem>
+              <NavItem eventKey={2} href="/About">
+                <h4> Par mani</h4>
+              </NavItem>{" "}
+              <NavItem
+                eventKey={3}
+                target="_blank"
+                href="https://twitter.com/sporto_vesels"
+                id="floatdiv2"
               >
-                Sporto Vesels <strong> - </strong>{" "}
-                <b style={{ color: "#026f89" }}>Dzīvo Labāk</b>
-              </h4>
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight className=" navbar-right">
-            <NavItem eventKey={1} href="/Trenini">
-              <h4>Individuālie treniņi</h4>
-            </NavItem>
-            <NavItem eventKey={3} href="/Konsultacijas">
-              <h4>Konsultācijas</h4>
-            </NavItem>
-            <NavItem eventKey={3} href="/Seminari">
-              <h4>Semināri</h4>
-            </NavItem>
-            <NavItem eventKey={2} href="/About">
-              <h4> Par mani</h4>
-            </NavItem>{" "}
-            <NavItem
-              eventKey={3}
-              target="_blank"
-              href="https://twitter.com/sporto_vesels"
-              id="floatdiv2"
-            >
-              <i className="fa fa-twitter color-twitter fa-2x" />
-            </NavItem>
-            <NavItem
-              eventKey={4}
-              target="_blank"
-              href="https://www.facebook.com/sporto.vesels"
-              id="floatdiv"
-            >
-              <i className="fa fa-facebook color-facebook fa-2x" />
-            </NavItem>
-            <NavItem
-              eventKey={5}
-              target="_blank"
-              href="mailto:fitnesakonsultants@inbox.lv"
-              id="floatdiv"
-            >
-              <i className="fa fa-envelope color-gmail fa-2x" />
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+                <i className="fa fa-twitter color-twitter fa-2x" />
+              </NavItem>
+              <NavItem
+                eventKey={4}
+                target="_blank"
+                href="https://www.facebook.com/sporto.vesels"
+                id="floatdiv"
+              >
+                <i className="fa fa-facebook color-facebook fa-2x" />
+              </NavItem>
+              <NavItem
+                eventKey={5}
+                target="_blank"
+                href="mailto:fitnesakonsultants@inbox.lv"
+                id="floatdiv"
+              >
+                <i className="fa fa-envelope color-gmail fa-2x" />
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div>
+          <h2
+            style={{
+              textAlign: "center",
+              color: "rgb(2, 111, 137)",
+              fontSize: "28px"
+            }}
+          >
+            Kā paaugstināt darbaspējas, atbrīvoties no liekajiem kilogramiem un
+            uzlabot savu dzīves kvalitāti
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
